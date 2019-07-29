6ef33012a95aa0a37b7b81ec4ec28a18d6eb493f
Aubrey Powell <herufeanor@gmail.com>
2019-07-28 19:15:54 -0700

    LET THERE BE LIGHT!
    Clock to 24 MHz, and feed bytes to the LEDs at the appropriate rate.

--- src/main.rs
+++ src/main.rs
@@ -6,37 +6,52 @@ extern crate panic_itm;
 use stm32f1xx_hal::{
     prelude::*,
     pac,
-    stm32::{ RCC, TIM2, tim2 }
 };
+use core::ptr;
 use cortex_m_rt::entry;
 
-fn delay(timer: &tim2::RegisterBlock, ms: u16) {
-    timer.arr.write(|w| w.arr().bits(ms));
-    timer.cr1.write(|w| w.cen().set_bit());
-    while !timer.sr.read().uif().bit_is_set() {}
-    timer.sr.write(|w| w.uif().clear_bit());
+const GPIOC_ODR: u32 = 0x4001100C;
+
+macro_rules! ws2812_byte {
+    ($byte:ident) => {
+        for _ in 0..8 {
+            let bit = (($byte as u32) & 0b1000_0000) >> 7;
+            unsafe { ptr::write_volatile(GPIOC_ODR as *mut u32, 1 << 13); }
+            cortex_m::asm::nop();
+            unsafe { ptr::write_volatile(GPIOC_ODR as *mut u32, bit << 13); }
+            cortex_m::asm::nop();
+            cortex_m::asm::nop();
+            unsafe { ptr::write_volatile(GPIOC_ODR as *mut u32, 0); }
+            cortex_m::asm::nop();
+            cortex_m::asm::nop();
+            $byte = $byte << 1;
+        }
+    };
 }
 
 #[entry]
 fn main() -> ! {
     let dp = pac::Peripherals::take().unwrap();
 
+    let mut flash = dp.FLASH.constrain();
     let mut rcc = dp.RCC.constrain();
+    let _ = rcc.cfgr.use_hse(8.mhz()).sysclk(24.mhz()).freeze(&mut flash.acr);
 
     let mut gpioc = dp.GPIOC.split(&mut rcc.apb2);
-    let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
-
-    let rcc = unsafe { &*RCC::ptr() };
-    let tim2 = unsafe { &*TIM2::ptr() };
-
-    rcc.apb1enr.write(|w| w.tim2en().set_bit());
-    tim2.cr1.write(|w| w.opm().set_bit().cen().clear_bit());
-    tim2.psc.write(|w| unsafe { w.psc().bits(7_199) });
+    let _ = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
 
     loop {
-        led.set_high();
-        delay(tim2, 1000);
-        led.set_low();
-        delay(tim2, 1000);
+        for _ in 0..144 {
+            let mut byte1: u8 = 16;
+            let mut byte2: u8 = 0;
+            let mut byte3: u8 = 0;
+            ws2812_byte!(byte1);
+            ws2812_byte!(byte2);
+            ws2812_byte!(byte3);
+        }
+
+        for _ in 0..200 {
+            cortex_m::asm::nop();
+        }
     }
 }
