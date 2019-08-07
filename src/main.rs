f5c4a011c76e10d8a1a4ef6c23389765d1a1a8ae
Aubrey Powell <herufeanor@gmail.com>
2019-08-07 00:07:15 -0700

    Add new flame pattern.

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ extern crate panic_itm;
 mod framebuffer;
 mod pattern;
 mod rainbow_pattern;
+mod flame_pattern;
 
 use nb::block;
 use stm32f1xx_hal::{
@@ -17,6 +18,7 @@ use cortex_m_rt::entry;
 use framebuffer::{ Framebuffer, FRAMEBUFFER_SIZE };
 use pattern::Pattern;
 use rainbow_pattern::RainbowPattern;
+use flame_pattern::FlamePattern;
 
 #[entry]
 fn main() -> ! {
@@ -30,15 +32,16 @@ fn main() -> ! {
 
     let mut framebuffer = Framebuffer::new(&mut rcc.apb2, dp.GPIOA);
     let mut frame = 0;
-    let mut pattern = RainbowPattern::new();
+    let mut rainbow_pattern = RainbowPattern::new();
+    let mut flame_pattern = FlamePattern::new();
 
     loop {
-        pattern.update();
+        rainbow_pattern.update();
 
         for i in 0..FRAMEBUFFER_SIZE {
             let x = i / 16;
-            let y = if x % 2 == 0 { i % 16 } else { 16 - (i % 16) };
-            let (r, g, b) = pattern.get_pixel(x, y, frame);
+            let y = if x % 2 == 0 { i % 16 } else { 15 - (i % 16) };
+            let (r, g, b) = rainbow_pattern.get_pixel(x, y, frame);
             framebuffer.set_pixel(i, r, g, b);
         }
 
